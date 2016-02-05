35205cd0a0044a8d3e9e47008ebd7aea48e29e7d
michael-conway <michael_conway@unc.edu>
2016-02-05 11:29:29 -0500

    #8 adding vc stuff

---
+++ src/main/java/org/irods/jargon/vircoll/exception/VirtualCollectionProfileException.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package org.irods.jargon.vircoll.exception;
+
+import org.irods.jargon.core.exception.JargonException;
+
+/**
+ * Exception generating or maintaining a user profile of virtual collections
+ * 
+ * @author Mike Conway - DICE
+ *
+ */
+public class VirtualCollectionProfileException extends JargonException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -810975433409675869L;
+
+	/**
+	 * @param message
+	 */
+	public VirtualCollectionProfileException(String message) {
+		super(message);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public VirtualCollectionProfileException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public VirtualCollectionProfileException(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param underlyingIRODSExceptionCode
+	 */
+	public VirtualCollectionProfileException(String message, Throwable cause,
+			int underlyingIRODSExceptionCode) {
+		super(message, cause, underlyingIRODSExceptionCode);
+	}
+
+	/**
+	 * @param cause
+	 * @param underlyingIRODSExceptionCode
+	 */
+	public VirtualCollectionProfileException(Throwable cause,
+			int underlyingIRODSExceptionCode) {
+		super(cause, underlyingIRODSExceptionCode);
+	}
+
+	/**
+	 * @param message
+	 * @param underlyingIRODSExceptionCode
+	 */
+	public VirtualCollectionProfileException(String message,
+			int underlyingIRODSExceptionCode) {
+		super(message, underlyingIRODSExceptionCode);
+	}
+
+}
