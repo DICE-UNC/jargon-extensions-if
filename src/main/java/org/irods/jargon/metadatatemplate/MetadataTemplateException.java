0318bc6cd5a8dd2988d5d70cde1f3c8af651b526
Mike Conway <mike.conway@nih.gov>
2018-07-17 14:50:13 -0400

    #25 fix missing exceptions

--- src/main/java/org/irods/jargon/metadatatemplate/MetadataTemplateException.java
+++ src/main/java/org/irods/jargon/metadatatemplate/MetadataTemplateException.java
@@ -4,4 +4,34 @@ import org.irods.jargon.core.exception.JargonException;
 
 public class MetadataTemplateException extends JargonException {
 
+	public MetadataTemplateException(String message, int underlyingIRODSExceptionCode) {
+		super(message, underlyingIRODSExceptionCode);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MetadataTemplateException(String message, Throwable cause, int underlyingIRODSExceptionCode) {
+		super(message, cause, underlyingIRODSExceptionCode);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MetadataTemplateException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MetadataTemplateException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MetadataTemplateException(Throwable cause, int underlyingIRODSExceptionCode) {
+		super(cause, underlyingIRODSExceptionCode);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MetadataTemplateException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
 }
