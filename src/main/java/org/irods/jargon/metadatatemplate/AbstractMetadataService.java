185f281148f32c3451fcde6d786a80d86ecdd488
patelhetalit <patelhetalit@gmail.com>
2018-07-24 16:31:29 -0400

    service

--- src/main/java/org/irods/jargon/metadatatemplate/AbstractMetadataService.java
+++ src/main/java/org/irods/jargon/metadatatemplate/AbstractMetadataService.java
@@ -9,12 +9,21 @@ public abstract class AbstractMetadataService {
 
 	public abstract UUID saveTemplate(MDTemplate metadataTemplate) throws MetadataTemplateException;
 
-	public abstract boolean deleteTemplateByUUID(String uniqueName) throws MetadataTemplateException;
+	public abstract boolean deleteTemplateByName(String uniqueName) throws MetadataTemplateException;
+	
+	public abstract boolean deleteTemplateByGuid(UUID guid) throws MetadataTemplateException;
+	
 	public abstract List<MDTemplate> listPublicTemplates();
 
 	public abstract List<MDTemplate> listAllTemplates(final String path) throws MetadataTemplateException;
 
 	public abstract MDTemplate findTemplateByUUID(UUID uuid)
+			throws MetadataTemplateNotFoundException, MetadataTemplateException;
+	
+	public abstract MDTemplate findTemplateByName(String templateName)
+			throws MetadataTemplateNotFoundException, MetadataTemplateException;
+	
+	public abstract boolean isTemplateExist(String templateName)
 			throws MetadataTemplateNotFoundException, MetadataTemplateException;
 
 	
